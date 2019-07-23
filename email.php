0c8fb536ee97ccff05b7be0efa93b7e45c2702de
Manohar N G <manohar.ng30@gmail.com>
2019-07-23 13:29:58 +0530

    added page name in email subject

--- email.php
+++ email.php
@@ -4,9 +4,9 @@ if(isset($_POST['submit']))
 	$name = $_POST['username'];
 	$email= $_POST['emailadd'];
 	$phone= $_POST['phoneno'];
-	var_dump($email);
-	$fromID="harish.dermsurg@gmail.com";
-	$toID = "harish.dermsurg@gmail.com";
+	$pageName = $_POST['pagename'];
+	$fromID="manoharg@suntechnologies.com";
+	$toID = "manoharg@suntechnologies.com";
 	//$toID = "chaitra.bejai11@gmail.com";
 	if(($_POST['phoneno'] == "")||($_POST['username'] == "")||($_POST['emailadd'] == ""))
 	{
@@ -17,14 +17,15 @@ if(isset($_POST['submit']))
 	}
 	else if(!preg_match("/^([A-z0-9]{1,})([\.|\_]{1})?([A-z0-9]{1,})\@([A-z]{2,7})(\.)([A-z]{3,4})$/",$_POST['emailadd'])) {
 		echo  "<script>alert('Kindly enter a valid email ID');window.history.go(-1);</script>";
-		header("Location: index.html");exit;
+		header("Location: home.html");exit;
 	}
 	else if(!preg_match("/^\d{10}$/",$_POST['phoneno'])) {
 		echo  "<script>alert('Kindly enter a valid Phone number');window.history.go(-1);</script>";
 		header("Location: index.html");exit;
 	}
 	else{
-	$subject= "New Lead from Vitals Klinic Page";
+		
+	$subject= "New Lead from Vitals Klinic".$pageName." Page";
 	$headers = "From: ".$fromID."\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
@@ -33,11 +34,11 @@ if(isset($_POST['submit']))
 Email ID: $email<br>
 Phone number: $phone</body></html>";
 	$result = mail($toID,  $subject, $message, $headers);
-	
-	var_dump($result);exit;
-	if(!$result)
+	}
+	if(!$result){
 		echo "<script>alert('Failed due to server error. Please try again later');location.href=\"index.html\"</script>";
-	else
+	}
+	else{
 		echo "<script>location.href=\"https://www.vitalsklinic.com/thankyou\"</script>";	
 	}
 }
